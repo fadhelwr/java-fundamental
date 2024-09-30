50fb71bc328b7fca0ed57b1707975c5104b272a9
Fadhel <fadhelwr@gmail.com>
2024-09-30 19:50:13 +0700

    create operator class

---
+++ src/main/java/com/juaracoding/Operator.java
@@ -0,0 +1,16 @@
+package com.juaracoding;
+
+public class Operator {
+    public static void main(String[] args) {
+        //aritmatik
+        double result = 5.0/2;
+        System.out.println(result);
+        int modulus = 8 % 2;
+        System.out.println(modulus);
+
+        int x = 2;
+        int hasil = 10 * x++;
+        System.out.println(hasil);
+
+    }
+}
