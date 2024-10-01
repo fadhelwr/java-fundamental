76ff177c351a220663f60957aa220fbef5a8cc9e
Fadhel <fadhelwr@gmail.com>
2024-10-01 18:36:19 +0700

    update code

---
+++ src/main/java/com/juaracoding/postTest.java
@@ -0,0 +1,17 @@
+package com.juaracoding;
+
+public class postTest {
+    public static void main(String[] args) {
+        String state = "on";
+
+        if(state == "on") {
+
+            System.out.println("On");
+
+        } else {
+
+            System.out.println("Off");
+
+        }
+    }
+}
